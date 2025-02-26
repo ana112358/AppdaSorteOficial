aac6fe1410f4071ab99b76e31bcf38ef5af096ae
Ana Paula <ana.paula.ufrn.ti@gmail.com>
2025-02-26 20:08:28 -0300

    fix: finalizei a lógica da geração de números, mas ainda com defeitos de layouts

--- AppdaSorteOficial/MainPage.xaml.cs
+++ AppdaSorteOficial/MainPage.xaml.cs
@@ -8,8 +8,30 @@ public partial class MainPage : ContentPage
 	}
 	private void GerarNumeros(object sender, EventArgs e)
 	{
-		NomeAPP.IsVisible = false;
-		ConteinerNumeros.IsVisible = true;
-	}
+        NomeAPP.IsVisible = false;
+        ConteinerNumeros.IsVisible = true;
+
+        var random = new Random();
+        var numerosSorte = new HashSet<int>();
+
+        while (numerosSorte.Count < 7)
+        {
+            numerosSorte.Add(random.Next(1, 61));  // Números de 1 a 60
+        }
+
+  
+        var numeroArray = new int[7];
+        numerosSorte.CopyTo(numeroArray);
+
+        numero01.Text = numeroArray[0].ToString("D2");
+        numero02.Text = numeroArray[1].ToString("D2");
+        numero03.Text = numeroArray[2].ToString("D2");
+        numero04.Text = numeroArray[3].ToString("D2");
+        numero05.Text = numeroArray[4].ToString("D2");
+        numero06.Text = numeroArray[5].ToString("D2");
+        numero07.Text = numeroArray[6].ToString("D2");
+
+
+    }
 	
 }
