dcb15b2dfb468e9ec197545dd8623955617cb0f1
Ana Paula <ana.paula.ufrn.ti@gmail.com>
2025-02-27 09:11:53 -0300

    feat: Mudança na estrutura da lógica de gerar número e na modularização das funções

--- AppdaSorteOficial/MainPage.xaml.cs
+++ AppdaSorteOficial/MainPage.xaml.cs
@@ -6,32 +6,38 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
-	private void GerarNumeros(object sender, EventArgs e)
+
+	private void OnGererateLuckNumbers(object sender, EventArgs e)
 	{
         NomeAPP.IsVisible = false;
         ConteinerNumeros.IsVisible = true;
 
-        var random = new Random();
-        var numerosSorte = new HashSet<int>();
+		var set = GerarNumeros();
 
-        while (numerosSorte.Count < 7)
-        {
-            numerosSorte.Add(random.Next(1, 61));  // Números de 1 a 60
-        }
+		numero01.Text = set.ElementAt(0).ToString("D2");
+		numero02.Text = set.ElementAt(1).ToString("D2");
+		numero03.Text = set.ElementAt(3).ToString("D2");
+	}
+	private static SortedSet<int> GerarNumeros()
+	{
+      
+		var set= new SortedSet<int>(); //Criei uma lista 
 
-  
-        var numeroArray = new int[7];
-        numerosSorte.CopyTo(numeroArray);
+        //HashSet
+        //
+        while (set.Count<6)
+		{
+			var random = new Random(); //Instanciando a Classe Random
+			var luckNumber = random.Next(1,60);
 
-        numero01.Text = numeroArray[0].ToString("D2");
-        numero02.Text = numeroArray[1].ToString("D2");
-        numero03.Text = numeroArray[2].ToString("D2");
-        numero04.Text = numeroArray[3].ToString("D2");
-        numero05.Text = numeroArray[4].ToString("D2");
-        numero06.Text = numeroArray[5].ToString("D2");
-        numero07.Text = numeroArray[6].ToString("D2");
+			set.Add(luckNumber);
+		};
+		return set;
+
+        
 
 
     }
+
 	
 }
